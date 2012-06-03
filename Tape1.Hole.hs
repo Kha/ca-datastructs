5f92509c2e5441b571308b48339e59c9eda23d01
Sebastian Ullrich <sebasti@nullrich.de>
2012-06-03 23:38:29 +0200

    Complete constant stack1

--- Tape1.Hole.hs
+++ Tape1.Hole.hs
@@ -6,11 +6,11 @@ module Tape1 (stack1) where
 import CA
 import Data.Char
 
-data Dir = L1 | L2 | N | R deriving (Eq,Show)
+data Dir = L Int | N | R deriving (Eq,Show)
 
 instance MultiShow (Char,Dir) where
-    multiShow (c,L1) = ["<"]
-    multiShow (c,L2) = ["«"]
+    multiShow (c,L 0) = ["<"]
+    multiShow (c,L _) = ["«"]
     multiShow (c,N) = multiShow c
     multiShow (c,R) = [[toUpper c]]
 
@@ -19,7 +19,7 @@ stack1 cmds = runStack1 (Automaton {
     delta = delta
     }) liftCmd cmds
     where
-        liftCmd Pop = (' ',L1)
+        liftCmd Pop = (' ',L 0)
         liftCmd Nop = (' ',R)
         liftCmd (Push c) = (c,R)
 
@@ -30,12 +30,10 @@ stack1 cmds = runStack1 (Automaton {
         delta1 (c1,d) (c2,N) _ | (c1 /= ' ' || d == R) && c2 /= ' ' = (c2,R) -- push source choose
         delta1 _ q1 _ = q1
 
-        delta2 (_,L1) (' ',_) (' ',_) = (' ',N)
-        delta2 (_,L1) (' ',_) (c,_) = (c,L1)
-        delta2 (_,L1) _ (c,_) = (c,N)
-        delta2 _ (c,L1) _ = (c,L2)
-        delta2 _ (_,L2) (' ',_) = (' ',L1)
-        delta2 _ (_,L2) (c,_) = (c,N)
+        delta2 (_,L 2) (' ',_) (' ',_) = (' ',N)
+        delta2 (_,L i) (' ',_) (' ',_) = (' ',L $ i+1)
+        delta2 (_,L _) _ _ = (' ',L 0)
+        delta2 _ (_,L _) (c,_) = (c,N)
         delta2 _ q1 _ = q1
 
         delta = delta2 `composeDelta` delta1 `composeDelta` delta0
